b77703e2cb241e997ddc2da542b90b20f03cc194
Manuel <majomeva011@gmial.com>
2021-10-01 19:04:59 -0500

    Empezando interfaz en react

---
+++ productos/src/components/BandaSuperior.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BandaSuperior() {
+    return (
+        <header className = "head">
+            <div> Mercado libre Changuero</div>
+        </header>
+    )
+}
+
+export default BandaSuperior
---
+++ productos/src/components/Entrada.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Entrada() {
+    return (
+        <div className = "buscarID">
+            <input type="text"
+            className = "entradaTest"
+            value = {'ID Producto'}
+            />
+            <button className = "botonBuscar">Buscar</button>
+        </div>
+    )
+}
+
+export default Entrada
