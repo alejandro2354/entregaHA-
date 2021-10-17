18bc0afcec9f5c22d02c3e172c185d008b225803
Kevin Gallardo <andreskevin2606@gmail.com>
2021-10-17 04:07:12 -0500

    Arreglando bugs pequeños

--- Frontend/src/components/UsuarioHeader.js
+++ Frontend/src/components/UsuarioHeader.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export const UsuarioHeader = ({ user }) => {
-    console.log(user);
     return (
         <div className="userDatos">
             <div className="datos">{user.rol}</div>
