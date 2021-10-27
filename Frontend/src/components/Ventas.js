00280fc16b8f0e9407909fbba4290fad3e8596a0
Kevin Gallardo <andreskevin2606@gmail.com>
2021-10-27 02:05:57 -0500

    habilite actualizar el producto en la venta

--- Frontend/src/components/Ventas.js
+++ Frontend/src/components/Ventas.js
@@ -250,7 +250,7 @@ function Ventas() {
                     </div>
                 </div>
             </div>
-            {ventas2.length > 0 ? <Listar ventas={ventas2} getVentas={() => getVentas()}/> : (buscarVenta === null || buscarVenta === "") ? <Listar ventas={ventas} getVentas={() =>getVentas()}/> : <h2 style={styles.mensajeNoSeEncontro}>No existen ventas registradas con id, nombre o cedula: {buscarVenta} </h2>}
+            {ventas2.length > 0 ? <Listar productos={productos} ventas={ventas2} getVentas={() => getVentas()}/> : (buscarVenta === null || buscarVenta === "") ? <Listar  productos={productos} ventas={ventas} getVentas={() =>getVentas()}/> : <h2 style={styles.mensajeNoSeEncontro}>No existen ventas registradas con id, nombre o cedula: {buscarVenta} </h2>}
         </Fragment>
     );
 }
